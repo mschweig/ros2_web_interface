ecfd39039a026ed51fb16a51b36c17e74ce8f236
Manuel Schweiger <schweigermanuel@icloud.com>
2025-04-22 16:06:25 +0200

    increase data timeout
    Signed-off-by: Manuel Schweiger <schweigermanuel@icloud.com>

--- ros2_web_interface/models.py
+++ ros2_web_interface/models.py
@@ -15,7 +15,7 @@ class GetDataQuery(BaseModel):
         example="/ros2_web_service_test/chatter"
     )
     timeout: float = Field(
-        10.0, 
+        15.0, 
         gt=0, 
         description="Timeout in seconds", 
         example=5.0
